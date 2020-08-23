731de84e3dab17e558f2c22bc62c45c9687cc7f2
doorwarpranjal <doorwarpranjal@gmail.com>
2020-08-24 02:31:19 +0530

    changed homepage image

--- src/pages/bloglist.js
+++ src/pages/bloglist.js
@@ -1,10 +1,10 @@
-import React from "react"
-import Helmet from 'react-helmet';
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
-import PostLink from "../components/post-link"
-import Navbar from '../components/navbar/Navbar'
-import blogStyles from "../styles/blogStyles.module.scss"
+import React from "react";
+import Helmet from "react-helmet";
+import { graphql } from "gatsby";
+import Layout from "../components/layout";
+import PostLink from "../components/post-link";
+import Navbar from "../components/navbar/Navbar";
+import blogStyles from "../styles/blogStyles.module.scss";
 
 const BlogList = ({
   data: {
@@ -12,30 +12,23 @@ const BlogList = ({
     allMarkdownRemark: { edges },
   },
 }) => {
-
-
-
   const Posts = edges
-    .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
-    .map(edge => <PostLink key={edge.node.id} post={edge.node} />)
+    .filter((edge) => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
+    .map((edge) => <PostLink key={edge.node.id} post={edge.node} />);
 
   return (
     <div>
       <Navbar />
-   
-   <div className={blogStyles.container}>
 
-      <h2 className={blogStyles.heading}>Blog Posts &darr;</h2>
-      <div className={blogStyles.blogsContainer}>
-        {Posts}
+      <div className={blogStyles.container}>
+        <h2 className={blogStyles.heading}>Blog Posts &darr;</h2>
+        <div className={blogStyles.blogsContainer}>{Posts}</div>
       </div>
-      </div>
-      </div>
-  )
-}
+    </div>
+  );
+};
 
-export default BlogList
-
+export default BlogList;
 
 export const pageQuery = graphql`
   query indexPageQuery {
@@ -61,4 +54,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,32 +1,23 @@
-import React from 'react'
-import '../styles/homepage.css'
-import Navbar from '../components/navbar/Navbar'
-import Layout from "../components/layout"
+import React from "react";
+import "../styles/homepage.css";
+import Navbar from "../components/navbar/Navbar";
+import Layout from "../components/layout";
 
 export default function Homepage() {
-    return (
-<Layout>
-   <Navbar />
-        <div className="homecontainer">
-            <div className="leftcontainer">
-            <h1 className="mainheading">The Future is Feminist.</h1>
-
-
-            <div className="line-home"></div>
-
-            <p className="mainpara">
-            It's not my responsibility to be beautiful. I'm not alive for that purpose. My existence is not about how desirable you find me.
-            </p>
-
-            <button className="cta-style">Explore</button>
-
-            </div>
-            <div className="rightcontainer">
-
-            </div>
+  return (
+    <Layout>
+      <Navbar />
+      <div className="homecontainer">
+        <div className="leftcontainer">
+          <h1 className="mainheading">The Future is Feminist.</h1>
+          <div className="line-home"></div>
+          <p className="mainpara">
+            My existence is not about how desirable you find me.
+          </p>
+          <button className="cta-style">Explore</button>
         </div>
-        </Layout>
-     
-       
-    )
+        <div className="rightcontainer"></div>
+      </div>
+    </Layout>
+  );
 }
