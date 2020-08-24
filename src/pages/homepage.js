d565529fb53f53f2988908f08535a80d68222568
doorwarpranjal <doorwarpranjal@gmail.com>
2020-08-24 13:09:14 +0530

    not anymore

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -13,7 +13,7 @@ export default function Homepage() {
           <div className="line-home"></div>
           <p className="mainpara">
             My existence is not about how desirable you find me.
-            It's about me.
+           Not Anymore.
           </p>
           <button className="cta-style">Explore</button>
         </div>
