6a71f891fa05b7299265c601fca94586444c261b
doorwarpranjal <doorwarpranjal@gmail.com>
2020-08-28 01:17:45 +0530

    tryyy

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -26,7 +26,7 @@ export default function Homepage() {
         </div>
         <div className="rightcontainer">
    
-          <img src={require('../images/OK.gif')} />
+          <img src={require('../../public/assets/OK.gif')} />
         </div>
       </div>
     </Layout>
