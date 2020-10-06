d37d5569b56c9747fb6960ece4d34e3f6baa9c7d
pranjal doorwar <doorwarpranjal@gmail.com>
2020-10-06 09:40:43 +0530

    deleted hometemp

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,29 +2,13 @@ import React from "react";
 import Helmet from "react-helmet";
 import { graphql } from "gatsby";
 
-// import "../js/jquery-3.3.1.min.js"
-// import "../js/bootstrap.min.js"
-// import "../js/modernizr.js"
-// import "../js/fullpage.min.js"
-// import "../js/scrolloverflow.js"
-// import "../js/demo3.js"
-// import "../js/easings.js"
-// import "../js/jquery.inview.min.js"
-// import "../js/jquery.countTo.js"
-// import "../js/owl.carousel.js"
-// import "../js/jquery.magnific-popup.js"
-// import "../js/custom_V.js"
-
-
-
-import Homepage from "../pages/Homepage";
+import Homepage from './Homepage'
 
 const IndexPage = () => {
   return (
     <div>
 
-      <Homepage />
-
+<Homepage />
     </div>
   );
 };
