05cb50a929123f9f2123f87bb686272fe484cb3e
Abhishek P <abhishek.p@piktorlabs.com>
2021-03-10 16:48:24 +0530

    Added arrows to the carousels

--- src/components/cards/editorcard.js
+++ src/components/cards/editorcard.js
@@ -1,58 +1,61 @@
-import React from "react";
-import { StaticQuery, graphql } from "gatsby";
-import PostLink2 from "../post-link2";
-import Slider from "react-slick";
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import PostLink2 from '../post-link2'
+import Slider from 'react-slick'
+
+import './latestblogcard.css'
 
 const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false
-};
+	dots: true,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 4000,
+}
 
 export default function Editorcard() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query editorCardQuery {
-          allMarkdownRemark(
-            sort: { order: DESC, fields: [frontmatter___date] }
-            limit: 6
-          ) {
-            edges {
-              node {
-                id
-                frontmatter {
-                  date(formatString: "MMMM DD, YYYY")
-                  path
-                  title
-                  thumbnail
-                  author
-                  category
-                  EditorChoice
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={data => (
-        <center>
-          <div className="container">
-            <div>
-              <Slider {...settings}>
-                {data.allMarkdownRemark.edges
-                  .filter(i => i.node.frontmatter.EditorChoice)
-                  .map(item => (
-                    <PostLink2 key={item.node.id} post={item.node} />
-                  ))}
-              </Slider>
-            </div>
-          </div>
-        </center>
-      )}
-    />
-  );
+	return (
+		<StaticQuery
+			query={graphql`
+				query editorCardQuery {
+					allMarkdownRemark(
+						sort: { order: DESC, fields: [frontmatter___date] }
+						limit: 6
+					) {
+						edges {
+							node {
+								id
+								frontmatter {
+									date(formatString: "MMMM DD, YYYY")
+									path
+									title
+									thumbnail
+									author
+									category
+									EditorChoice
+								}
+							}
+						}
+					}
+				}
+			`}
+			render={(data) => (
+				<center>
+					<div className='container slide-show-container'>
+						<div>
+							<Slider {...settings}>
+								{data.allMarkdownRemark.edges
+									.filter((i) => i.node.frontmatter.EditorChoice)
+									.map((item) => (
+										<PostLink2 key={item.node.id} post={item.node} />
+									))}
+							</Slider>
+						</div>
+					</div>
+				</center>
+			)}
+		/>
+	)
 }
--- src/components/cards/latestblogcard.js
+++ src/components/cards/latestblogcard.js
@@ -1,66 +1,58 @@
-import React from "react";
-import { StaticQuery, graphql } from "gatsby";
-import PostLink2 from "../post-link2";
-import Slider from "react-slick";
+import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
+import PostLink2 from '../post-link2'
+import Slider from 'react-slick'
+
+import './latestblogcard.css'
 
 const settings = {
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false
-};
+	dots: true,
+	infinite: true,
+	speed: 500,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+	autoplaySpeed: 4000,
+}
 
 export default function Latestblogcard() {
-  return (
-    <StaticQuery
-      query={graphql`
-        query headingQueryAndHeadingQuer {
-          allMarkdownRemark(
-            sort: { order: DESC, fields: [frontmatter___date] }
-            limit: 6
-          ) {
-            edges {
-              node {
-                id
-                frontmatter {
-                  date(formatString: "MMMM DD, YYYY")
-                  path
-                  title
-                  thumbnail
-                  author
-                  category
-                }
-              }
-            }
-          }
-        }
-      `}
-      render={data => (
-        //           <Slider {...settings}>
-        //             <div>
-
-        //             {data.allMarkdownRemark.edges.map((item)=>{
-
-        //               return  <PostLink2 key={item.node.id} post={item.node} />
-
-        //             })}
-
-        // </div>
-        // </Slider>
-        <center>
-          <div className="container">
-            <div>
-              <Slider {...settings}>
-                {data.allMarkdownRemark.edges.map(item => {
-                  return <PostLink2 key={item.node.id} post={item.node} />;
-                })}
-              </Slider>
-            </div>
-          </div>
-        </center>
-      )}
-    />
-  );
+	return (
+		<StaticQuery
+			query={graphql`
+				query headingQueryAndHeadingQuer {
+					allMarkdownRemark(
+						sort: { order: DESC, fields: [frontmatter___date] }
+						limit: 6
+					) {
+						edges {
+							node {
+								id
+								frontmatter {
+									date(formatString: "MMMM DD, YYYY")
+									path
+									title
+									thumbnail
+									author
+									category
+								}
+							}
+						}
+					}
+				}
+			`}
+			render={(data) => (
+				<center>
+					<div className='container slide-show-container'>
+						<div>
+							<Slider {...settings}>
+								{data.allMarkdownRemark.edges.map((item) => {
+									return <PostLink2 key={item.node.id} post={item.node} />
+								})}
+							</Slider>
+						</div>
+					</div>
+				</center>
+			)}
+		/>
+	)
 }
