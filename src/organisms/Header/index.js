0957bb5cb0993c22ced63662eb6f64fabd6503a9
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-25 23:23:53 +0530

    Header navigation added

--- src/organisms/Header/index.js
+++ src/organisms/Header/index.js
@@ -7,15 +7,18 @@ import styles from './index.module.css'
 import LOGO from '../../images/banner/Logo.png'
 import SEARCH from '../../images/banner/search.svg'
 import NavBar from '../../molecules/NavBar'
+import { Link } from 'gatsby'
 
 const Header = () => {
 	return (
 		<header className={styles.container}>
-			<div className='flex-row-space-between'>
-				<img className={styles.logo} src={LOGO} alt='logo'></img>
-				<Spacer x={20} />
-				<h1 className={styles.title}>the intersectional feminist</h1>
-			</div>
+			<Link to='/'>
+				<div className='flex-row-space-between'>
+					<img className={styles.logo} src={LOGO} alt='logo'></img>
+					<Spacer x={20} />
+					<h1 className={styles.title}>the intersectional feminist</h1>
+				</div>
+			</Link>
 			<div className='flex-row-space-between'>
 				<NavBar />
 				<Spacer x={30} />
