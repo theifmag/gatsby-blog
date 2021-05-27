8e21afe052e377260f1dbf0e60574a85c56ca6b9
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-27 11:10:57 +0530

    Header cleaned for all resolutions

--- src/molecules/NavBar/index.js
+++ src/molecules/NavBar/index.js
@@ -2,23 +2,25 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Spacer from '../../atoms/Spacer'
+import useWindowResize from '../../functions/useWindowResize'
 
 import styles from './index.module.css'
 
 const NavBar = () => {
+	const [mobile] = useWindowResize()
 	return (
-		<div className='flex-row'>
+		<div className={['flex-row', styles.container].join(' ')}>
 			<Link className={styles.link} activeClassName={styles.activeLink} to='/'>
 				Home
 			</Link>
-			<Spacer x={40} />
+			<Spacer x={mobile ? 20 : 40} />
 			<Link
 				activeClassName={styles.activeLink}
 				className={styles.link}
 				to='/articles'>
 				Articles
 			</Link>
-			<Spacer x={40} />
+			<Spacer x={mobile ? 20 : 40} />
 			<Link
 				activeClassName={styles.activeLink}
 				className={styles.link}
--- src/organisms/Header/index.js
+++ src/organisms/Header/index.js
@@ -1,30 +1,35 @@
 import React from 'react'
-
-import Spacer from '../../atoms/Spacer'
-
-import styles from './index.module.css'
-
-import LOGO from '../../images/banner/Logo.png'
-import SEARCH from '../../images/banner/search.svg'
-import NavBar from '../../molecules/NavBar'
 import { Link } from 'gatsby'
 
+import Spacer from '../../atoms/Spacer'
+import NavBar from '../../molecules/NavBar'
+import useWindowResize from '../../functions/useWindowResize'
+
+import styles from './index.module.css'
+import LOGO from '../../images/banner/Logo.png'
+import SEARCH from '../../images/banner/search.svg'
+
 const Header = () => {
+	const [mobile] = useWindowResize()
+
 	return (
-		<header className={styles.container}>
-			<Link to='/'>
+		<>
+			<header className={styles.container}>
+				<Link to='/'>
+					<div className='flex-row-space-between'>
+						<img className={styles.logo} src={LOGO} alt='logo'></img>
+						<Spacer x={mobile ? 10 : 20} />
+						<h1 className={styles.title}>the intersectional feminist</h1>
+					</div>
+				</Link>
 				<div className='flex-row-space-between'>
-					<img className={styles.logo} src={LOGO} alt='logo'></img>
-					<Spacer x={20} />
-					<h1 className={styles.title}>the intersectional feminist</h1>
+					{mobile || <NavBar />}
+					{mobile || <Spacer x={30} />}
+					<img className={styles.searchIcon} src={SEARCH} alt='search'></img>
 				</div>
-			</Link>
-			<div className='flex-row-space-between'>
-				<NavBar />
-				<Spacer x={30} />
-				<img className={styles.searchIcon} src={SEARCH} alt='search'></img>
-			</div>
-		</header>
+			</header>
+			{mobile && <NavBar />}
+		</>
 	)
 }
 
