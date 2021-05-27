066389b1d52708b1c23ad6a2bd1812e8d3a88bef
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-27 19:31:10 +0530

    All updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,6 @@
 module.exports = {
 	/* Your site config here */
 	siteMetadata: require('./site-meta-data.json'),
-	aboutPage: require('./aboutPage.json'),
 	plugins: [
 		{
 			resolve: `gatsby-source-filesystem`,
