5e12cb57097fb88bf8002de4d4a5496c39205c2f
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-28 19:09:24 +0530

    Added carousel

--- src/organisms/BannerCarousel/index.js
+++ src/organisms/BannerCarousel/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Carousel } from 'react-responsive-carousel'
+import 'react-responsive-carousel/lib/styles/carousel.min.css' // requires a loader
 
 import styles from './index.module.css'
 
@@ -6,7 +8,16 @@ import CAROUSEL_IMAGE from '../../images/banner/carousel-1.jpg'
 
 const BannerCarousel = () => (
 	<div className={styles.container}>
-		<img className={styles.image} src={CAROUSEL_IMAGE} alt='carousel' />
+		<Carousel
+			autoPlay
+			emulateTouch
+			infiniteLoop
+			showThumbs={false}
+			showStatus={false}>
+			<img className={styles.image} src={CAROUSEL_IMAGE} alt='carousel' />
+			<img className={styles.image} src={CAROUSEL_IMAGE} alt='carousel' />
+			<img className={styles.image} src={CAROUSEL_IMAGE} alt='carousel' />
+		</Carousel>
 	</div>
 )
 
