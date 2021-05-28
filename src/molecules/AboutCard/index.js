6b5c76e5324df8ce0d60b01bb937cc4a06550404
Abhishek P <abhishek.p@piktorlabs.com>
2021-05-28 11:03:01 +0530

    Deployed color color change

--- src/molecules/AboutCard/index.js
+++ src/molecules/AboutCard/index.js
@@ -4,7 +4,17 @@ import Spacer from '../../atoms/Spacer'
 
 import styles from './index.module.css'
 
-const colorColor = ['#aab8a0', '#ffcfa0', '#eac38d']
+const colorColor = [
+	'#fcc09b',
+	'#9ed692',
+	'#a7b4f5',
+	'#a7b4f5',
+	'#fcc09b',
+	'#9ed692',
+	'#9ed692',
+	'#a7b4f5',
+	'#fcc09b',
+]
 
 const AboutCard = (props) => {
 	const { name, bio, thumbnail, index } = props
@@ -13,7 +23,7 @@ const AboutCard = (props) => {
 		<div className={styles.container}>
 			<div
 				className={styles.background}
-				style={{ backgroundColor: colorColor[index % 3] }}></div>
+				style={{ backgroundColor: colorColor[index] }}></div>
 			<img className={styles.thumbnail} src={thumbnail} alt={name} />
 			<Spacer y={15} />
 			<h2 className={styles.title}>{name}</h2>
