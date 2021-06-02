aeea2d13e33ab82518368b446ffe2209774b1e5b
Abhishek P <abhishek.p@piktorlabs.com>
2021-06-02 13:54:42 +0530

    Moved social links to CMS

--- src/molecules/ContactLinks/index.js
+++ src/molecules/ContactLinks/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import {graphql, useStaticQuery} from 'gatsby';
 import Spacer from '../../atoms/Spacer';
 import SocialImageLink from '../../atoms/SocialImageLink';
 
@@ -9,36 +10,56 @@ import FACEBOOK_LOGO from '../../images/social/facebook.svg';
 import INSTAGRAM_LOGO from '../../images/social/instagram.svg';
 import LINKEDIN_LOGO from '../../images/social/linkedin.svg';
 
-const ContactLinks = () => (
-  <div className={styles.container}>
-    <h2>Contact</h2>
-    <Spacer y={16} />
-    <a className={styles.email} href="mailto:theifmag@gmail.com">
-      theifmag@gmail.com
-    </a>
-    <Spacer y={40} />
-    <h2>Follow</h2>
-    <Spacer y={16} />
-    <div className={styles.socialIconsContainer}>
-      <SocialImageLink
-        src={FACEBOOK_LOGO}
-        alt="Facebook logo"
-        link="https://www.fb.com/theifmag"
-      />
-      <Spacer x={20} />
-      <SocialImageLink
-        src={INSTAGRAM_LOGO}
-        alt="Instagram logo"
-        link="https://www.instagram.com/theifmag"
-      />
-      <Spacer x={20} />
-      <SocialImageLink
-        src={LINKEDIN_LOGO}
-        alt="Linkedin logo"
-        link="https://www.linkedin.com/in/theifmag"
-      />
+const ContactLinks = () => {
+  const data = useStaticQuery(graphql`
+    query FooterContactQuery {
+      site {
+        siteMetadata {
+          socialLinks {
+            linkedIn
+            email
+            instagram
+            facebook
+          }
+        }
+      }
+    }
+  `);
+
+  const {linkedIn, email, instagram, facebook} =
+    data.site.siteMetadata.socialLinks;
+
+  return (
+    <div className={styles.container}>
+      <h2>Contact</h2>
+      <Spacer y={16} />
+      <a className={styles.email} href={`mailto:${email}`}>
+        theifmag@gmail.com
+      </a>
+      <Spacer y={40} />
+      <h2>Follow</h2>
+      <Spacer y={16} />
+      <div className={styles.socialIconsContainer}>
+        <SocialImageLink
+          src={FACEBOOK_LOGO}
+          alt="Facebook logo"
+          link={facebook}
+        />
+        <Spacer x={20} />
+        <SocialImageLink
+          src={INSTAGRAM_LOGO}
+          alt="Instagram logo"
+          link={instagram}
+        />
+        <Spacer x={20} />
+        <SocialImageLink
+          src={LINKEDIN_LOGO}
+          alt="Linkedin logo"
+          link={linkedIn}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default ContactLinks;
